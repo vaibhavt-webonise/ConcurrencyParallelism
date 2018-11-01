f03d3d1b8c0d07824af065785384d11253b4cf8c
Vaibhav Tapadiya <vaibhav.tapadiya@weboniselab.com>
2018-11-01 17:58:26 +0530

    ConCurencyAssignmentSubmission

--- DinningPhilospher.java
+++ DinningPhilospher.java
@@ -1,23 +1,23 @@
 public class DinningPhilospher{
- 
+
     public static void main(String[] args) throws Exception {
- 	
+
         Philosopher[] philosophers = new Philosopher[5];
         Object[] forks = new Object[5];
- 
-        for (int i = 0; i < forks.length; i++) {
-            forks[i] = new Object();
+
+        for (int counter = 0; counter < forks.length; counter++) {
+            forks[counter] = new Object();
         }
- 
-        for (int i = 0; i < philosophers.length; i++) {
-            Object leftFork = forks[i];
-            Object rightFork = forks[(i + 1) % forks.length];
- 
-            philosophers[i] = new Philosopher(forks[i], forks[(i + 1) % 5]);
-             
-            Thread t 
-              = new Thread(philosophers[i], "Philosopher " + (i + 1));
-            t.start();
+
+        for (int counter = 0; counter < philosophers.length; counter++) {
+            Object leftFork = forks[counter];
+            Object rightFork = forks[(counter + 1) % forks.length];
+
+            philosophers[counter] = new Philosopher(forks[counter], forks[(counter + 1) % 5]);
+
+            Thread philosophersTeam
+              = new Thread(philosophers[counter], "Philosopher " + (counter + 1));
+            philosophersTeam.start();
         }
     }
 }
